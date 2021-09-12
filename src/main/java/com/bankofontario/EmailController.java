f87da5ce2bd9ee02c904f9537deb7152b8b947fd
Ramesh <rao9536@gmail.com>
2021-09-12 18:16:14 -0400

    email issue fixed

--- src/main/java/com/bankofontario/EmailController.java
+++ src/main/java/com/bankofontario/EmailController.java
@@ -4,19 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class EmailController {
 
 
-    String [] recepients = new String[]{"sugardaddy647@gmail.com"};
-
     @RequestMapping(value = "/sendemail", method = RequestMethod.POST)
-    public boolean sendEmail( String UserName,String UserComments ) {
+    public boolean sendEmail(String UserName, String UserComments, String email) {
 
+        List<String> emailList = new ArrayList<String>();
+        emailList.add(email);
 
         String [] bccRecepients =new String[]{"sugardaddy647@gmail.com"};
 
-        boolean response = new SendEmail().sendMail(recepients, bccRecepients, "Account details and activation confirmation",
+        boolean response = new SendEmail().sendMail(emailList, bccRecepients, "Account details and activation confirmation",
                 UserComments,"sugardaddy647@gmail.com", "Ramesh@98s",UserName);
         return response;
     }
